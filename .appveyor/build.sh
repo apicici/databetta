bae183e879749edcf3d6d3cab698ea032eb57464
apicici <apicici@apicici.com>
2022-02-12 16:19:37 -0500

    update

--- .appveyor/build.sh
+++ .appveyor/build.sh
@@ -3,12 +3,12 @@ LSQLITE_VER="0.9.5-1"
 LSQLITE_UUID="fsl09y"
 
 
-pip install hererocks
+pip3 install hererocks
 hererocks env --lua 5.1 -rlatest
 source env/bin/activate
 luarocks install moonscript
 
-wget "http://lua.sqlite.org/index.cgi/zip/lsqlite3_${LSQLITE_UUID}.zip"
+curl "http://lua.sqlite.org/index.cgi/zip/lsqlite3_${LSQLITE_UUID}.zip"
 unzip "lsqlite3_${LSQLITE_UUID}.zip"
 cd "lsqlite3_${LSQLITE_UUID}"
 
