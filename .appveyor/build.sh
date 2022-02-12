79d0fc100ec497fcb40cf32b714e44f4434efa54
apicici <apicici@apicici.com>
2022-02-12 16:21:36 -0500

    fix curl

--- .appveyor/build.sh
+++ .appveyor/build.sh
@@ -8,8 +8,8 @@ hererocks env --lua 5.1 -rlatest
 source env/bin/activate
 luarocks install moonscript
 
-curl "http://lua.sqlite.org/index.cgi/zip/lsqlite3_${LSQLITE_UUID}.zip"
-unzip "lsqlite3_${LSQLITE_UUID}.zip"
+curl "http://lua.sqlite.org/index.cgi/zip/lsqlite3_${LSQLITE_UUID}.zip" --output lsqlite3.zip
+unzip lsqlite3.zip
 cd "lsqlite3_${LSQLITE_UUID}"
 
 luarocks make "lsqlite3complete-${LSQLITE_VER}.rockspec" --no-install --lua-version 5.1
